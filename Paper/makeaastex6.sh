088bf957f5cb7fd85d9c85da1df9b0459d452c22
T. E. Rivera-Thorsen (utah) <trive@astro.su.se>
2017-01-23 20:51:01 +0100

    First revision submitted
    Incorporated comments from Göran and Matthew plus referee report, see
    details in the newly added coverletter.* files.

--- Paper/makeaastex6.sh
+++ Paper/makeaastex6.sh
@@ -1,9 +1,9 @@
-scholdoc Paper.md \
---template=pandoc-aastex6.latex \
---natbib \
--t latex -o Paper.tex
-
-pdflatex Paper.tex
-bibtex Paper
-pdflatex Paper.tex
-
+##  scholdoc Paper.md \
+##  --template=pandoc-aastex6.latex \
+##  --natbib \
+##  -t latex -o Paper.tex
+##  
+##  pdflatex Paper.tex
+##  bibtex Paper
+##  pdflatex Paper.tex
+##  
---
+++ Paper/tester.dm
@@ -0,0 +1,14 @@
+# A test
+
+another test 
+------------
+
+
+``` python
+import numpy as np
+
+def my_function(in1, in2=None):
+    if in2 is not None:
+        in1 += 'Hallå-der!'
+    return
+```
